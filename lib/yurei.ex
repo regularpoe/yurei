d429ebd14c0a36deef45f8996da3ba8132debf8b
regularpoe <vedranvinko@protonmail.com>
2024-08-11 12:34:47 +0200

    Implement simple echo TCP server

--- lib/yurei.ex
+++ lib/yurei.ex
@@ -1,18 +1,32 @@
 defmodule Yurei do
-  @moduledoc """
-  Documentation for `Yurei`.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info("Accepting connections on port #{port}..")
+    loop_acceptor(socket)
+  end
 
-  ## Examples
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    serve(client)
+    loop_acceptor(socket)
+  end
 
-      iex> Yurei.hello()
-      :world
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
 
-  """
-  def hello do
-    :world
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
   end
 end
